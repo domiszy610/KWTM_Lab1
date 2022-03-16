6f60fbb4ba5253a8cbea79b458648ce2eb430c60
Dominika Szyma <opos113@gmail.com>
2022-03-16 11:39:35 +0100

    Build with use trigonometry OFF

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,18 @@
-#include "trygonometria.h"
+#include "LaborkaConfig.h"
+
+#ifdef USE_TRIGONOMETRY_DEGREE
+	#include "trygonometria.h"
+#else
+	#include "cmath"
+#endif
+
 int main(){
+
+#ifdef USE_TRIGONOMETRY_DEGREE
+ 	double result = cos_degree(45.0);
+#else
+	double pi = 3.14159265358979323846;
+	double result = cos(pi/4.0);
+#endif
 return 0;
 }
